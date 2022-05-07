eca611956631b191207c3a662d5280afbba0f9c0
LukeP <95398114+timeToC0de@users.noreply.github.com>
2022-05-06 23:58:06 -0400

    Update board.h

--- board.h
+++ board.h
@@ -84,19 +84,30 @@ public:
 // Board which contains the functions to do the rendering of the game board using an array of rows, rows are just arrays of boardSquares.
 // I
 class board {
+
 #define BSIZE = 8
+
 public:
 
 	const static int bSize = 8; // Board size.
 	row chessBoard[bSize]; // Is a dynamic array of rows which are just arrays of boardSquares of size 8.
 	HANDLE console_color;  // Creates a HANDLE object, console_color, that will then be passed, along with a color code, to the SetConsoleTextAttribute function to change the console's and text colors as desired by us.
-	
-						   //vector<row>::iterator chessBoardPtr; // Exploring the idea of using vectors to store the rows.
+				
+	void printInstructionMenu();
+	void printPawnInstruction();
+	void printRookInstruction();
+	void printKnightInstruction();
+	void printBishopInstruction();
+	void printQueenInstruction();
+	void printKingInstruction();
+	void printListOfCommands();
 
 	void movePiece(int row, int column, char pieceName); // Used to transfer the piece from its current square to the designated square.
 
 	board() { // Default constructor.
 		int boardColor;
+
+		startGame(); // Used to see if the user wants to see a list of rules and instructions for the game.
 		initColorHandler(); // Initializes color handler so that it won't be redeclared during every function call.
 		
 		initBoard(); // Initializes the board so it has the pieces in the appropriate places for the start of the game.
@@ -114,6 +125,7 @@ public:
 	}
 
 private:
+	void startGame(); // Used to prompt the user if they would like to see the instructions.
 	void printHorizontalLines(); // Prints the horizontal lines of the game board.
 	void printWhiteBoard(int& bColor);
 	void printRow(int rowNumber, int borderColor);
@@ -124,6 +136,124 @@ private:
 	int chooseBoardBoundaryColor();
 };
 
+
+// Used to inform the users about the game and to print the initial board.
+void board::startGame() {
+	int inputCounter = 0; // Used to prevent an infinite loop if the user fails to enter valid input.
+	bool isMenuPrinted = false;
+	char instructionChoice; // Used to control whether the instructions are printed.
+
+	do {
+		cout << "Would you like to view the instructions for the CLI chess game? y/n? ";
+		cin >> instructionChoice; // Allows user to enter y or n to display the menu or not.
+		inputCounter++; // Keeps track of the number of times that the user has input 
+		instructionChoice = tolower(instructionChoice);
+		if (instructionChoice == 'y' || instructionChoice == 'n') {
+			if (instructionChoice == 'n') {
+				return; // Exits the function since user responded no.
+			}
+			else
+				printInstructionMenu(); // Prints the instruction menu.
+			isMenuPrinted = true; // Causes input loop to exit.
+
+		}
+		else if (inputCounter > 6) { // If the loop runs 5 times or more.
+			cout << "Improper input entered 5 times.\n"\
+				<< "Program is closing.\n";
+			system("pause"); // Pauses so the user can read the message above.
+			exit(1); // Exits the program with exit code 1.
+		}
+		else {
+			cout << "Y or N are the only valid options. Please re-enter your choice.\n";
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+		}
+		cout << "\n";
+
+	} while (!isMenuPrinted); // Runs until the user enters y or n. May exit if the user fails to enter valid input for 5 iterations.
+	return; // Passes control back to calling function.
+}
+
+// Prints the instructions.
+void board::printInstructionMenu() {
+	cout << "\nThis chess game is played turn by turn between 2 players.\n"
+		<< "The player with the white pieces moves first.\n"
+		<< "Rank is the term for a particular row of the board, and are numbered from 1 to 8.\n"
+		<< "File is the term for a column(vertical line of squares) and are labeled a to h.\n"
+		<< "To move a piece, input the Piece name, "
+		<< "the square the piece is on, and the square that you want the piece to go.\n"
+		<< "For example, if you want to move the white pawn on a2 to a4, you would enter Pa2 a4\n"
+		<< "Enter the command in all caps or all lowercase, but don't put spaces before or after, only between the two squares.\n"
+		<< "You can't move to a square occupied by one of your pieces with any other piece.\n"
+		<< "The game board does not have coloring for the white or dark squares because the background colors\n"
+		<< "available for the CLI don't mingle well with the color of the 'pieces'.\n\n";
+	system("pause"); // Pauses the output so that the previous paragraph will appear and it won't just display the whole instruction set immediately.
+	cout << "\nThere are 6 types of pieces, each piece is represented with an abbreviation of their name.\n"
+		<< "The pieces for 1 player are 8 Pawns(P), 2 Castles(C), 2 Knights(N), 2 Bishops(B), 1 Queen(Q), and 1 King(K).\n\n";
+		printPawnInstruction();
+		printRookInstruction();
+		printKnightInstruction();
+		printBishopInstruction();
+		printQueenInstruction();
+		printKingInstruction();
+	system("pause");
+	cout << "\nFinally, I will describe how to win, how to resign, how to draw, what a stalemate is, and castling.\n"
+		<< "To win, you must put the other player's king in checkmate.\n"
+		<< "Checkmate means that your king or the opposing king is in a square currently being attacked, \n"
+		<< "and has no other square to move to that isn't being attacked or occupied by one of your own pieces.\n"
+		<< "To resign, instead of a move, enter r into the move prompt.\n"
+		<< "To offer a draw, enter d into the move prompt and the opposing player will have the option to \n"
+		<< "enter y for yes I accept or n for no I decline the draw.\n"
+		<< "If a draw is declined, then the game will resume.\n"
+		<< "A stalemate is when one player has no valid moves to make.\n"
+		<< "If all pawns are blocking each other and your king is locked in, then that will result in a stalemate.\n"
+		<< "Castling can only be done using the king and one of the castles.\n"
+		<< "Castling is when you move your king 2 squares towards your rook,\nthe rook will be placed on the square to the other side of the direction of your king's movement.\n"
+		<< "Castling can only be done if the king and the particular castle have not yet moved.\n"
+		<< "Remember, you can't castle once the King has moved or if the particular rook has moved.\n"
+		<< "White's pieces are displayed with the white color and Black's pieces are displayed in gray.\n"
+		<< "\nIllegal moves will be rejected and you will be asked to move again.\n"
+		<< "To see these rules again during the chess session, simply type i and hit enter.\n\n";
+	system("pause");
+}
+
+void board::printPawnInstruction() {
+	cout << "Pawns may move forward and not backwards. They may move 2 squares forwards if the particular pawn hasn't moved yet.\n"
+		<< "Once the pawn has moved, it may only move 1 square forwards at a time.\n"
+		<< "Pawns may only take a piece that is 1 square in front of them on one of the 2 adjacent diagonals.\n"
+		<< "Pawns may not take the piece directly in front of them.\n"
+		<< "Pawns have a special condition called En Passant.\n"
+		<< "En Passant is the situation when a pawn is on the 4th rank if the pawn is black\n"
+		<< "or the 5th rank if the pawn is white and the opposing player's pawn moves 2 squares landing on the adjacent square\n"
+		<< "to your pawn, your pawn may capture this pawn only on the same turn as the opposing player's pawn move.\n"
+		<< "After the capture, your pawn will be one rank towards the opposing player's side\nof the board and a square behind where the opposing pawn had just moved.\n"
+		<< "Once one of your pawns get across the board to the final rank of the opposing side,\n"
+		<< "the pawn will be promoted which means that you may choose it to become a castle, knight, bishop, or queen.\n\n";
+}
+void board::printRookInstruction() {
+	cout << "Castles(aka rooks) are simple, they may move vertically or horizontally up to the square adjacent to one of your pieces.\n"
+		<< "If it is an opposing piece, you can have your castle move to their square to capture it if lies on the same rank or file.\n\n";
+} 
+void board::printKnightInstruction() {
+	cout << "The knights are special. They are the only piece that can jump over other pieces.\n"\
+		<< "Knights can move in an L shape, that means that they can move to any square\ntwo forwards and one horizontal from their current location.\n"
+		<< "They can not land on one of your own pieces though.\n\n";
+}
+void board::printBishopInstruction() {
+	cout << "Bishops are also very simple, they are like castles but they may only move diagonally instead.\n\n";
+}
+void board::printQueenInstruction() {
+	cout << "The Queen is able to move like both the castle and the bishop.\n";
+}
+void board::printKingInstruction() {
+	cout << "The King may move to any square adjacent to his current position.\n"
+		<< "The King can't move into check and any particular piece of yours\nis not allowed to make a move resulting in your king being in check.\n\n";
+}
+
+void board::printListOfCommands() {
+	cout << "The list of command you may enter into the move prompt are: ";
+}
+
 // Allows the user to choose the color of the board.
 int board::chooseBoardBoundaryColor() {
 	int colorChoice; // Stores the color choice of the user.
@@ -132,7 +262,7 @@ int board::chooseBoardBoundaryColor() {
 	do {
 		
 		
-		cout << "Select the color that you want the board to be:\n"\
+		cout << "Now select the color that you want the chess board's borders to be:\n"\
 			<< "1 - Blue\n"\
 			<< "2 - Green\n"\
 			<< "3 - Aqua\n"\
@@ -146,8 +276,8 @@ int board::chooseBoardBoundaryColor() {
 			<< "11 - Light Aqua\n"\
 			<< "12 - Light Red\n"\
 			<< "13 - Light Purple\n"\
-			<< "14 - Light Yellow\n"\
-			<< "15 - Bright White\n"\
+			<< "14 - Light Yellow\n"
+			<< "15 - Bright White\n"
 			<< "Enter one of the color codes: ";
 
 		cin >> colorChoice;
@@ -296,8 +426,8 @@ void board::movePiece(int row, int column, char pieceName) {
  
 }
 
-#endif //!H_board
 
+// Contains used, old, or potentially useful code.
 /*
 				case 1:
 					break;
